5f947ab15471411f4ca6f56a06154cdf71a26d7d
Joe Bowen <joe.b.bowen@gmail.com>
2020-10-20 21:31:27 -0600

    Fixing bugs with remote control

--- remote/remote_main.py
+++ remote/remote_main.py
@@ -8,7 +8,28 @@ import sys
 from libraries.launch_control import LaunchControl
 
 
+class StreamToLogger(object):
+    """ Fake file-like stream object that redirects writes to a logger instance.
+    """
+
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass
+
+
 logging.basicConfig(level=logging.INFO, format='(%(threadName)-9s) %(message)s', )
+
+stdout_logger = logging.getLogger('STDOUT')
+sl = StreamToLogger(stdout_logger, logging.INFO)
+sys.stdout = sl
 
 
 @click.command()
