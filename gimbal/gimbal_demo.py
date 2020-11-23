1c4501c0d403944c8472c0b4846b30b99a554af8
Joe Bowen <joe.b.bowen@gmail.com>
2020-11-23 08:42:08 -0700

    Added a demo script for the camera gimbal

---
+++ gimbal/gimbal_demo.py
@@ -0,0 +1,23 @@
+import time
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels=16)
+
+kit.servo[14].angle = 0
+kit.servo[15].angle = 90
+time.sleep(5)
+while True:
+    kit.servo[15].angle = 90
+    time.sleep(3)
+    kit.servo[15].angle = 110
+    time.sleep(3)
+    kit.servo[15].angle = 70
+    time.sleep(3)
+    kit.servo[14].angle = 0
+    time.sleep(1)
+    kit.servo[14].angle = 180
+    time.sleep(.5)
+    kit.servo[14].angle = 180
+    time.sleep(3)
+    kit.servo[14].angle = 0
+    time.sleep(3)
