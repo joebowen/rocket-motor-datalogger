3c937928c94f3f2f459b8605b9c3e7ef533eb885
Joe Bowen <joe.b.bowen@gmail.com>
2020-11-23 18:15:32 -0700

    Added a demo script for the camera gimbal

--- gimbal/gimbal_demo.py
+++ gimbal/gimbal_demo.py
@@ -18,7 +18,9 @@ def main():
         time.sleep(3)
 
         gimbal.pitch_up(percentage=100)
-        time.sleep(3)
+        time.sleep(1)
+        gimbal.yaw_to_angle(angle=70)
+        time.sleep(4)
         gimbal.pitch_down(percentage=100)
         time.sleep(3)
 
