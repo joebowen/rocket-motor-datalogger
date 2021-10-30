be743126f4341b17e292545b5414fc72ba1f3485
Joe Bowen <joe.b.bowen@gmail.com>
2021-10-29 18:32:36 -0600

    Debugging issue with radios

--- common/comms.py
+++ common/comms.py
@@ -26,7 +26,7 @@ class Comms:
 
     def set_config(self):
         print(self.interface.localNode.radioConfig)
-        print(dir(self.interface.localNode.radioConfig))
+        print(dir(self.interface.localNode.channels))
         self.interface.localNode.radioConfig.preferences.is_low_power = False
         self.interface.localNode.radioConfig.preferences.is_router = False
 
